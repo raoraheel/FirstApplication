5dd0db34282b3c1886780734c1eeaf59d6e4dec1
raoraheel <raoraheel@gmail.com>
2018-09-21 00:09:27 -0500

    test project

---
+++ src/FirstApplication/Calc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstApplication;
+
+/**
+ *
+ * @author raora
+ */
+public class Calc {
+    
+    int a, b, c, d;
+    
+    public Calc(int a, int b)
+    {
+    super();
+    c = a * b;
+    System.out.println("Product of a and b is:"+c);
+    }
+}
